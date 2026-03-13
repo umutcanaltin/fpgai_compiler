e9c49bedbc30cc7a98c7f7f784104b006c1bc49f
Umut Can ALTIN <umutcanaltin1@gmail.com>
2026-03-13 01:00:05 +0100

    sigmoid fix

--- fpgai/backends/hls/emit/layers_activations.py
+++ fpgai/backends/hls/emit/layers_activations.py
@@ -2,57 +2,73 @@ def emit_activations_h() -> str:
     return r'''
 #pragma once
 #include "fpgai_types.h"
-#include <hls_math.h> // For exp()
+#include <hls_math.h>
 
 namespace fpgai {
 
 template<int N>
 void relu_inplace(act_t x[N]) {
-    for(int i=0; i<N; i++) {
-        #pragma HLS PIPELINE
-        if(x[i] < 0) x[i] = 0;
+    for (int i = 0; i < N; i++) {
+#pragma HLS PIPELINE II=1
+        if (x[i] < (act_t)0) {
+            x[i] = (act_t)0;
+        }
     }
 }
 
 template<int N>
-void leaky_relu_inplace(act_t x[N], float alpha) {
-    act_t a = (act_t)alpha;
-    for(int i=0; i<N; i++) {
-        #pragma HLS PIPELINE
-        if(x[i] < 0) x[i] = x[i] * a;
+void leaky_relu_inplace(act_t x[N], act_t alpha) {
+    for (int i = 0; i < N; i++) {
+#pragma HLS PIPELINE II=1
+        if (x[i] <= (act_t)0) {
+            x[i] = (act_t)(x[i] * alpha);
+        }
     }
 }
 
-// Softmax (casts to float for accuracy, then back)
+template<int N>
+void sigmoid_inplace(act_t x[N]) {
+    for (int i = 0; i < N; i++) {
+#pragma HLS PIPELINE II=1
+        float xf = (float)x[i];
+        float yf = 1.0f / (1.0f + hls::expf(-xf));
+        x[i] = (act_t)yf;
+    }
+}
+
 template<int N>
 void softmax_inplace(act_t x[N]) {
     float tmp[N];
-    float max_val = -1e9f;
+    float max_val = -1e30f;
 
-    // 1. Find Max (for numerical stability)
-    for(int i=0; i<N; i++) {
+    for (int i = 0; i < N; i++) {
+#pragma HLS PIPELINE II=1
         float f = (float)x[i];
-        if(f > max_val) max_val = f;
         tmp[i] = f;
+        if (f > max_val) {
+            max_val = f;
+        }
     }
 
-    // 2. Exp and Sum
     float sum = 0.0f;
-    for(int i=0; i<N; i++) {
-        #pragma HLS PIPELINE
-        tmp[i] = hls::exp(tmp[i] - max_val);
+    for (int i = 0; i < N; i++) {
+#pragma HLS PIPELINE II=1
+        tmp[i] = hls::expf(tmp[i] - max_val);
         sum += tmp[i];
     }
 
-    // 3. Normalize and Write Back
-    for(int i=0; i<N; i++) {
-        #pragma HLS PIPELINE
-        x[i] = (act_t)(tmp[i] / sum);
+    float inv_sum = 1.0f / sum;
+    for (int i = 0; i < N; i++) {
+#pragma HLS PIPELINE II=1
+        x[i] = (act_t)(tmp[i] * inv_sum);
     }
 }
 
-} // namespace
+} // namespace fpgai
 '''
 
+
 def emit_activations_cpp() -> str:
-    return '#include "layers/activations.h"\n'
+    return r'''
+#include "layers/activations.h"
+'''
